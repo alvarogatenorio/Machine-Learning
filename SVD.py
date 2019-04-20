0b12b61eb0c759badcd35e23f9ae0c84c782244c
bserranoanton <belen523@gmail.com>
2019-04-21 01:07:03 +0200

    comentarios SVD

--- SVD.py
+++ SVD.py
@@ -16,34 +16,38 @@ class SVD:
         
     #computes the SVD of our matrix of images
     def computeSVD(self):
+       #mean of our data 
        Xmean = np.mean(self.digits, axis = 1)
        X = self.digits - Xmean[:, np.newaxis]
+       #compute the SVD of X = points - meanOfPoints
        U, D2, Vt = np.linalg.svd(X, full_matrices=False)
        return U, D2, Vt,Xmean
 
-    #compute min d such that (....)
+    #compute d = the smallest dimension such that the amount of information 
+    #that is discarded is less than eps
     def computeDprime(self,D2, dim,eps):
+        threshold = 1 - eps
         for d in range(dim):
-            if(sum(D2[0:d]) / np.sum(D2) > 0.8):
+            if(sum(D2[0:d]) / np.sum(D2) > threshold):
                 return d
     
-    #compute the projection of the point point (number of the image)
-    #def computeProjection(point,Dprime,U,Xmean):
-     #   P = U[:,:Dprime].dot(U[:,:Dprime].T)
-     #   img = self.digits[:,point]
-     #   img_proj = Xmean + P.dot(img-Xmean)
-     #   return img_proj
-    
+    #compute the projection of the point num_image (number of the image)
     def reduccionDim(self,d,num_image,U,Xmean):
+        # dim(P) = dxd -> dimension reduction
         P = U[:,:d].dot(U[:,:d].T)
-        #num_image = 56745
+        #point 
         img = self.digits[:,num_image]
+        #poroyection of the point
         img_proj = Xmean + P.dot(img-Xmean)
         return [img,img_proj]
     
+    #prints the given point
     def show_digit(self,dgt):
         return plt.imshow(dgt.reshape((28,28)),cmap='gray')
     
+    #plots a sequence of 10 random numbers. The first row has the proyection
+    #of the numbers in the second row. The dimension of the proyection starts in
+    #10 and doubles its value in each iteration
     def plotPoints(self,N,U,Xmean):
         fig = plt.figure() #la caja que recoge todas las imágenes que ponemos dentro 
         fig_size = 10
@@ -56,7 +60,9 @@ class SVD:
             ax = fig.add_subplot(2,10,k)
             self.show_digit(solu[1])
             d = d+d
-            
+    
+    #plots a 6x10 grid. Each colum has the proyection of a random number, 
+    #the dimension of the proyection inscreases from top to bottom
     def plotGrid(self, N,U,Xmean):
         fig = plt.figure()
         fig.subplots_adjust(hspace=0.4, wspace=0.4)
--- mainSVD.py
+++ mainSVD.py
@@ -12,7 +12,9 @@ import SVD as classSVD
 import numpy as np
 import matplotlib.pyplot as plt
 
+#threshold to compute d (dimension of the proyection)
 eps = 0.8
+#dimension of the points and number of points
 D , N = 784, 70000
 
 #Read data from file
@@ -20,12 +22,13 @@ myData = classSVDio.SVDio('C:\hlocal\mldata\mldata\mnist-original.mat')
 myDigits = myData.importData()
 
 
-#....
 mySVD = classSVD.SVD(myDigits)
 U, D2, Vt, Xmean = mySVD.computeSVD()
 
 d = mySVD.computeDprime(D2,D,eps)
 
+print("La mínima dimensión d es: " +  str(d) )
+   
 #mySVD.plotPoints(N,U,Xmean)
 
 mySVD.plotGrid(N,U,Xmean)
